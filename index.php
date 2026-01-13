4daf49bc43b2167b248e9c4a83896a31b76eae5b
Emeric <102652005+CyREME@users.noreply.github.com>
2026-01-13 04:50:42 +0000

    Logo

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Outils</title>
 
     <link rel="stylesheet" href="Assets/css/style.css">
-
+    <link rel="icon" href="Assets/Img/logo.svg" type="image/svg+xml">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
 </head>
 <body>
 
