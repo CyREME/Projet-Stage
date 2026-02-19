18b80a1f531cd8b7dcf6afdcefcdbf403c467b4c
CyREME <emericcellier9740@gmail.com>
2026-02-19 09:49:23 +0400

    Mise à jour de la clé de chiffrement AES-256

--- Assets/Fonction/Config.php
+++ Assets/Fonction/Config.php
@@ -1,5 +1,5 @@
 <?php
 // Clé de 32 caractères pour AES-256
 // CHANGEZ CETTE VALEUR par une chaîne aléatoire complexe
-define('ENCRYPTION_KEY', 'votre_cle_secrete_aleatoire_32_chars');
+define('ENCRYPTION_KEY', 'bV9zYjYmNypAIVpLdzJRM3U1eTh4eiYh');
 ?>
